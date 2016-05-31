a1f35de6965183c198b74c6601a20724ffbe25d2
funzone36 <funzone36@gmail.com>
2016-05-31 17:48:40 -0400

    Added an if statement for bananas.

--- exercise6.rb
+++ exercise6.rb
@@ -2,7 +2,14 @@
 
 def output
   grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+  number_of_items = grocery_list.length
   grocery_list.each { |x| puts x}
+  puts "You have a total of #{number_of_items} items."
+  if grocery_list.include?("bananas") then
+    puts "You need to pick up bananas."
+  else
+    puts "You don't need to pick up bananas today."
+  end
 end
 
 output
