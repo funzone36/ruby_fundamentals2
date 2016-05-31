f701489226037f2f7bc4e0028656476e40a4c04e
funzone36 <funzone36@gmail.com>
2016-05-31 17:56:09 -0400

    sorted the list alphabetically

--- exercise6.rb
+++ exercise6.rb
@@ -2,15 +2,16 @@
 
 def output
   grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+  grocery_list.sort!{|firstGrocery,secondGrocery| firstGrocery <=>secondGrocery}
   number_of_items = grocery_list.length
-  grocery_list.each { |x| puts x}
+  grocery_list.each { |x| puts "*" + x}
   puts "You have a total of #{number_of_items} items."
   if grocery_list.include?("bananas") then
     puts "You need to pick up bananas."
   else
     puts "You don't need to pick up bananas today."
   end
-  puts grocery_list[1]
+  puts "*" + grocery_list[1]
 end
 
 output
