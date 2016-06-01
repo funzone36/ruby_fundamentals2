7e0df6868b04b9628280e282581b982ddaa3e7e0
funzone36 <funzone36@gmail.com>
2016-06-01 13:04:09 -0400

    Created a method to output the number of students in each cohort.

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,15 @@
+def students
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+students.each do |cohort, size|
+  puts "#{cohort}: #{size} students"
+
+end
+
+end
+students
